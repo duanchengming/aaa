0f1ba7192c4082d646a84185ba33c4da613ef959
duan <754021200@qq.com>
2019-06-10 13:54:27 +0800

    修改

--- git_test/src/main/java/com/duan/test/Test.java
+++ git_test/src/main/java/com/duan/test/Test.java
@@ -9,4 +9,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaa");
     }
+    public void test(){
+        System.out.println("22222222222222");
+    }
 }
